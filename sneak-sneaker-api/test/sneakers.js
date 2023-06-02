c369a9e79c1c47baac076bfb99ab5b33bdaebe41
I494715 <i494715@fhict.nl>
2023-06-02 10:58:21 +0200

    Fixed test yaml file

--- sneak-sneaker-api/test/sneakers.js
+++ sneak-sneaker-api/test/sneakers.js
@@ -1,30 +1,18 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const createServer = require('../index'); // Import the createServer function
 const should = chai.should();
 
 chai.use(chaiHttp);
 
+const host = 'http://localhost:3000'
+
 describe('Sneakers Get All test\n', function() {
-    let server;
-    before(async function() {
-        const app = await createServer();
-        server = app.listen();
-    });
-
-    after(function() {
-        server.close();
-    });
-
     it('should list all sneakers on /sneakers GET', function(done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .get('/sneakers')
             .end(function(err, res){
                 res.should.have.status(200);
-                
                 done();
             });
     });
-    
 });
-
